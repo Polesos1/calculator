3e1ba498bbfba6f3ae39e1614d576b3b67d2d8b8
Polesos1 <gaverdov.pavel@gmail.com>
2022-05-28 16:17:50 +0300

    calculator
    calculator

---
+++ src/CalculatorException.java
@@ -0,0 +1,12 @@
+
+
+public class CalculatorException extends Exception {
+    public CalculatorException() {
+        System.out.println("Не правильно задано выражение");
+    }
+
+    public CalculatorException(String message) {
+        this();
+        System.out.println(message);
+    }
+}
